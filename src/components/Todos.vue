ab4c291f49b577a61eff16c67604ede42561d387
AndrewMartial <andrewomosh44@gmail.com>
2022-05-05 18:24:20 +0300

    first commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div v-bind:key="todo.id" v-for="todo in todos">
+            <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/>
+            <!-- <h3>{{todo.title}}</h3> -->
+        </div>
+    </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+    name: 'Todos',
+    props: ['todos'],
+    components: {
+        TodoItem
+    },
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
